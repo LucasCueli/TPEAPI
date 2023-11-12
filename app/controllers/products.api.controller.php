e6eb09ef6f684fc9a4a96d98047d1059564ed6c0
Lucas Cueli <lucascueli4@gmail.com>
2023-11-12 17:55:03 -0300

    Error 400 añadido

--- app/controllers/products.api.controller.php
+++ app/controllers/products.api.controller.php
@@ -13,7 +13,15 @@
         //controllers para procesadores
 
         function obtenerProcesadores($params = []){
-            $procesadores = $this->model->getAllProcesadores();
+            if (isset($_GET['sort'])){
+                $params['sort'] = $_GET['sort'];
+                if (isset($_GET['order'])){
+                    $params['order'] = $_GET['order'];
+                }
+                $procesadores = $this->model->getAllProcesadoresOrder($params);
+            } else if(empty($params)){
+                $procesadores = $this->model->getAllProcesadores();
+            }
 
             return $this->view->response($procesadores, 200);
         }
@@ -45,9 +53,21 @@
             $body = $this->getData();
 
             $marca = $body->Marca;
+            if (empty($body->Marca)){
+                return $this->view->response("no ingreso marca del procesador.", 400);
+            }
             $modelo = $body->Modelo;
+            if (empty($body->Modelo)){
+                return $this->view->response("no ingreso modelo del procesador.", 400);
+            }
             $socket = $body->Socket;
+            if (empty($body->Socket)){
+                return $this->view->response("no ingreso socket del procesador.", 400);
+            }
             $valor = $body->Valor; 
+            if (empty($body->Valor)){
+                return $this->view->response("no ingreso valor del procesador.", 400);
+            }
 
             $this->model->addProcesador($marca,$modelo,$socket,$valor);
             $this->view->response("El procesador fue guardado exitosamente.", 201);
@@ -61,9 +81,21 @@
                 $body = $this->getData();
 
                 $marca = $body->Marca;
+                if (empty($body->Marca)){
+                    return $this->view->response("No ingreso marca del procesador.", 400);
+                }
                 $modelo = $body->Modelo;
+                if (empty($body->Modelo)){
+                    return $this->view->response("No ingreso modelo del procesador.", 400);
+                }
                 $socket = $body->Socket;
+                if (empty($body->Socket)){
+                    return $this->view->response("No ingreso socket del procesador.", 400);
+                }
                 $valor = $body->Valor; 
+                if (empty($body->Valor)){
+                    return $this->view->response("No ingreso valor del procesador.", 400);
+                } 
 
                 $this->model->updateProcesador($idProcesador, $marca, $modelo, $socket, $valor);
                 $this->view->response("El procesador con la ID={$idProcesador} ha sido actualizado.", 200);
@@ -75,8 +107,16 @@
         //controllers para graficas
 
         function obtenerGraficas($params = []){
-            $graficas = $this->model->getAllGraficas();
-
+            if (isset($_GET['sort'])){
+                $params['sort'] = $_GET['sort'];
+                if (isset($_GET['order'])){
+                    $params['order'] = $_GET['order'];
+                }
+                $graficas = $this->model->getAllGraficasOrder($params);
+            } else if (empty($params)){
+                $graficas = $this->model->getAllGraficas();
+            }
+            
             return $this->view->response($graficas, 200);
         }
 
@@ -107,9 +147,21 @@
             $body = $this->getData();
 
             $marca = $body->Marca;
+            if (empty($body->Marca)){
+                return $this->view->response("No ingreso marca de la grafica", 400);
+            }
             $modelo = $body->Modelo;
+            if (empty($body->Modelo)){
+                return $this->view->response("No ingreso modelo de la grafica", 400);
+            }
             $vram = $body->Vram;
+            if (empty($body->Vram)){
+                return $this->view->response("No ingreso vram de la grafica", 400);
+            }
             $valor = $body->Valor; 
+            if (empty($body->Valor)){
+                return $this->view->response("No ingreso valor de la grafica", 400);
+            }
 
             $this->model->addGrafica($marca,$modelo,$vram,$valor);
             $this->view->response("La grafica fue guardada exitosamente.", 201);
@@ -123,9 +175,21 @@
                 $body = $this->getData();
 
                 $marca = $body->Marca;
+                if (empty($body->Marca)){
+                    return $this->view->response("No ingreso marca de la grafica", 400);
+                }
                 $modelo = $body->Modelo;
+                if (empty($body->Modelo)){
+                    return $this->view->response("No ingreso modelo de la grafica", 400);
+                }
                 $vram = $body->Vram;
+                if (empty($body->Vram)){
+                    return $this->view->response("No ingreso vram de la grafica", 400);
+                }
                 $valor = $body->Valor; 
+                if (empty($body->Valor)){
+                    return $this->view->response("No ingreso valor de la grafica", 400);
+                }
 
                 $this->model->updateGrafica($idGrafica, $marca, $modelo, $vram, $valor);
                 $this->view->response("La grafica con la ID={$idGrafica} ha sido actualizada.", 200);
@@ -137,8 +201,16 @@
         //controllers para rams
 
         function obtenerRams($params = []){
-            $rams = $this->model->getAllRams();
-
+            if (isset($_GET['sort'])){
+                $params['sort'] = $_GET['sort'];
+                if (isset($_GET['order'])){
+                    $params['order'] = $_GET['order'];
+                }
+                $rams = $this->model->getAllRamsOrder($params);
+            } else if (empty($params)){
+                $rams = $this->model->getAllRams();
+            }
+        
             return $this->view->response($rams, 200);
         }
 
@@ -165,15 +237,29 @@
             }
         }
         
-        #ID_RAM, Marca, Tamaño, Velocidad, Generacion, Valor
         function agregarRam($params= []){
             $body = $this->getData();
 
             $marca = $body->Marca;
+            if (empty($body->Marca)){
+                return $this->view->response("No ingreso marca de la ram", 400);
+            }
             $tamaño = $body->Tamaño;
+            if (empty($body->Tamaño)){
+                return $this->view->response("No ingreso tamaño de la ram", 400);
+            }
             $velocidad = $body->Velocidad;
+            if (empty($body->Velocidad)){
+                return $this->view->response("No ingreso velocidad de la ram", 400);
+            }
             $generacion = $body->Generacion;
+            if (empty($body->Generacion)){
+                return $this->view->response("No ingreso generacion de la ram", 400);
+            }
             $valor = $body->Valor; 
+            if (empty($body->Valor)){
+                return $this->view->response("No ingreso valor de la ram", 400);
+            }
 
             $this->model->addRam($marca,$tamaño,$velocidad,$generacion,$valor);
             $this->view->response("La ram fue guardada exitosamente.", 201);
@@ -187,10 +273,25 @@
                 $body = $this->getData();
 
                 $marca = $body->Marca;
+                if (empty($body->Marca)){
+                    return $this->view->response("No ingreso marca de la ram", 400);
+                }
                 $tamaño = $body->Tamaño;
+                if (empty($body->Tamaño)){
+                    return $this->view->response("No ingreso tamaño de la ram", 400);
+                }
                 $velocidad = $body->Velocidad;
+                if (empty($body->Velocidad)){
+                    return $this->view->response("No ingreso velocidad de la ram", 400);
+                }
                 $generacion = $body->Generacion;
+                if (empty($body->Generacion)){
+                    return $this->view->response("No ingreso generacion de la ram", 400);
+                }
                 $valor = $body->Valor; 
+                if (empty($body->Valor)){
+                    return $this->view->response("No ingreso valor de la ram", 400);
+                }
 
                 $this->model->updateRam($idRam,$marca,$tamaño,$velocidad,$generacion,$valor);
                 $this->view->response("La ram con la ID={$idRam} ha sido actualizada.", 200);
@@ -202,7 +303,15 @@
         //controllers para gabinetes
 
         function obtenerGabinetes($params = []){
-            $gabinetes = $this->model->getAllGabinetes();
+            if (isset($_GET['sort'])){
+                $params['sort'] = $_GET['sort'];
+                if (isset($_GET['order'])){
+                    $params['order'] = $_GET['order'];
+                }
+                $gabinetes = $this->model->getAllGabinetesOrder($params);
+            } else if (empty($params)){
+                $gabinetes = $this->model->getAllGabinetes();
+            }
 
             return $this->view->response($gabinetes, 200);
         }
@@ -234,12 +343,24 @@
             $body = $this->getData();
 
             $marca = $body->Marca;
+            if (empty($body->Marca)){
+                return $this->view->response("No ingreso marca del gabinete", 400);
+            }
             $modelo = $body->Modelo;
+            if (empty($body->Modelo)){
+                return $this->view->response("No ingreso modelo del gabinete", 400);
+            }
             $tamaño = $body->Tamaño;
+            if (empty($body->Tamaño)){
+                return $this->view->response("No ingreso tamaño del gabinete", 400);
+            }
             $valor = $body->Valor; 
+            if (empty($body->Valor)){
+                return $this->view->response("No ingreso valor del gabinete", 400);
+            }
 
-            $this->model->addGabinete($marca,$modelo,$vram,$valor);
-            $this->view->response("La gabinete fue guardado exitosamente.", 201);
+            $this->model->addGabinete($marca,$modelo,$tamaño,$valor);
+            $this->view->response("El gabinete fue guardado exitosamente.", 201);
         }
 
         function actualizarGabinete($params= []){
@@ -250,9 +371,21 @@
                 $body = $this->getData();
 
                 $marca = $body->Marca;
+                if (empty($body->Marca)){
+                    return $this->view->response("No ingreso marca del gabinete", 400);
+                }
                 $modelo = $body->Modelo;
+                if (empty($body->Modelo)){
+                    return $this->view->response("No ingreso modelo del gabinete", 400);
+                }
                 $tamaño = $body->Tamaño;
+                if (empty($body->Tamaño)){
+                    return $this->view->response("No ingreso tamaño del gabinete", 400);
+                }
                 $valor = $body->Valor; 
+                if (empty($body->Valor)){
+                    return $this->view->response("No ingreso valor del gabinete", 400);
+                }
 
                 $this->model->updateGabinete($idGrafica, $marca, $modelo, $vram, $valor);
                 $this->view->response("El gabinete con la ID={$idGabinete} ha sido actualizado.", 200);
--- app/views/products.api.view.php
+++ app/views/products.api.view.php
@@ -11,6 +11,7 @@
             $status = array(
                 200 => "OK",
                 201 => "Created",
+                400 => "Bad Request",
                 404 => "Not Found",
                 500 => "Internal Server Error"
             );
